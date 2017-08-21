0a6600d30b752917fbb386bc1f195494f71a93d4
Okhrymenko Sergey <mr.inceon@gmail.com>
2017-08-21 23:38:55 +0300

    added russian locale

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Пароль должен содержать не менее шести символов и совпадать с подтверждением.',
+    'reset' => 'Ваш пароль сброшен!',
+    'sent' => 'Cсылка на восстановление пароля отправлена на електронную почту!',
+    'token' => 'Этот ссылка является недействительной.',
+    'user' => 'Пользователь с таким адресом электронной почты не обнаружен.',
+
+];
